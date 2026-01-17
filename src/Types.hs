cfb32491c924ab3a116b1e81c506ef7bf02d1a02
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2026-01-17 23:38:46 +0000

    Define core Message and Topic data types

--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,23 @@
-module Types where
+module Types 
+    ( Message(..)
+    , MessageType(..)
+    , Topic
+    ) where
 
--- Day 1: Foundation
--- This module is currently a placeholder for the Domain Model.
+type Topic = String
+
+-- | Type of message sent between users.
+data MessageType 
+    = Regular           -- ^ Standard chat/gossip
+    | FriendRequest     -- ^ Urgent request to connect
+    | FriendAccept      -- ^ Confirmation of connection
+    deriving (Show, Eq)
+
+-- | The core Message payload.
+data Message = Message
+    { sender     :: String
+    , content    :: String
+    , msgType    :: MessageType
+    , msgTopic   :: Topic
+    , msgSecrets :: [String] -- ^ Piggybacked gossip
+    } deriving (Show, Eq)
