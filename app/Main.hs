c388ab8f5ffab44d936bc711d6bdd1d62bdfcb07
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2026-01-21 01:46:23 +0000

    Wire Main to Run simulation entry point

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,11 @@
 module Main (main) where
 
 import Config (defaultConfig)
-import Control.Concurrent.STM (atomically, newTVarIO, readTVar)
-import qualified Data.Map as Map
-import Env (Env (..))
 import Logger (logMsg)
-import Types (User (..))
+import Run (runSimulation)
 
 main :: IO ()
 main = do
   logMsg "Monadic Social Network Simulation: Initializing..."
-
-  -- Verify Domain Model
-  print defaultConfig
-  logMsg "Domain Model Verification: Success"
+  runSimulation defaultConfig
+  logMsg "Simulation Finished."
